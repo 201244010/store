643acfc470bfbc4b32b9dd70770fa6121be1fb6c
Konata9 <maochichen@sunmi.com>
2019-04-19 19:01:08 +0800

    1. 添加登录后查看商户列表逻辑 2. 增加商户列表逻辑

--- src/models/Merchant/merchant.js
+++ src/models/Merchant/merchant.js
@@ -1,11 +1,12 @@
 import * as Actions from '@/services/Merchant/merchant';
 import { ERROR_OK } from '@/constants/errorCode';
-// import router from 'umi/router';
+import router from 'umi/router';
 
 export default {
     namespace: 'merchant',
     state: {
-        companyList: {
+        companyList: [],
+        companyInfo: {
             company_no: '123456',
             company_name: '星巴克上海管理有限公司',
             contact_person: '叶宿',
@@ -23,6 +24,21 @@ export default {
         // //   if (response && response.code === ERROR_OK) {
         // //   }
         // },
+        *getCompanyList(_, { call, put }) {
+            const response = yield call(Actions.getCompanyList);
+            if (response && response.code === ERROR_OK) {
+                const result = response.data || {};
+                yield put({
+                    type: 'updateState',
+                    payload: {
+                        companyList: result.company_list || [],
+                    },
+                });
+            } else {
+                router.push('/user/login');
+            }
+            return response;
+        },
 
         *companyGetInfo({ payload }, { call, put }) {
             const { options } = payload;
