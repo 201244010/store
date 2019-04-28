ef873242e434c52c6d4a6a901ae87195dc72af7a
Konata9 <maochichen@sunmi.com>
2019-04-28 10:30:55 +0800

    1. 修改价格显示，强制显示两位小数

--- src/pages/BasicData/ProductManagement/SearchResult.js
+++ src/pages/BasicData/ProductManagement/SearchResult.js
@@ -81,6 +81,7 @@ class SearchResult extends Component {
             {
                 title: formatMessage({ id: 'basicData.product.price' }),
                 dataIndex: 'price',
+                render: text => <span>{parseFloat(text).toFixed(2)}</span>,
             },
             {
                 title: formatMessage({ id: 'basicData.product.modified_time' }),
