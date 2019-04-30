ab5344390d0063d2ad3ab698377b327734d22efd
Konata9 <maochichen@sunmi.com>
2019-04-30 10:08:01 +0800

    1. 隐去邮箱相关信息

--- src/pages/User/ResetPassword/ResetPassword.js
+++ src/pages/User/ResetPassword/ResetPassword.js
@@ -1,37 +1,37 @@
 import React, { Component } from 'react';
 import { formatMessage, getLocale } from 'umi/locale';
-import { Divider } from 'antd';
-import BigIcon from '../../../components/BigIcon';
+// import { Divider } from 'antd';
+// import BigIcon from '../../../components/BigIcon';
 import MobileReset from './MobileReset';
 import MailReset from './MailReset';
 import styles from './ResetPassword.less';
 
-const ResetInit = props => {
-    const { switchStep } = props;
-    return (
-        <>
-            <div className={styles['reset-content']}>
-                <div className={styles['bigIcon-wrapper']} onClick={() => switchStep('toMobile')}>
-                    <BigIcon {...{ type: 'mobile' }} />
-                    <div className={styles['icon-description']}>
-                        {formatMessage({ id: 'reset.by.mobile' })}
-                    </div>
-                </div>
-                <Divider
-                    type="vertical"
-                    dashed
-                    style={{ height: '90px', borderRight: '1px dashed #e8e8e8' }}
-                />
-                <div className={styles['bigIcon-wrapper']} onClick={() => switchStep('toMail')}>
-                    <BigIcon {...{ type: 'mail' }} />
-                    <div className={styles['icon-description']}>
-                        {formatMessage({ id: 'reset.by.mail' })}
-                    </div>
-                </div>
-            </div>
-        </>
-    );
-};
+// const ResetInit = props => {
+//     const { switchStep } = props;
+//     return (
+//         <>
+//             <div className={styles['reset-content']}>
+//                 <div className={styles['bigIcon-wrapper']} onClick={() => switchStep('toMobile')}>
+//                     <BigIcon {...{ type: 'mobile' }} />
+//                     <div className={styles['icon-description']}>
+//                         {formatMessage({ id: 'reset.by.mobile' })}
+//                     </div>
+//                 </div>
+//                 <Divider
+//                     type="vertical"
+//                     dashed
+//                     style={{ height: '90px', borderRight: '1px dashed #e8e8e8' }}
+//                 />
+//                 <div className={styles['bigIcon-wrapper']} onClick={() => switchStep('toMail')}>
+//                     <BigIcon {...{ type: 'mail' }} />
+//                     <div className={styles['icon-description']}>
+//                         {formatMessage({ id: 'reset.by.mail' })}
+//                     </div>
+//                 </div>
+//             </div>
+//         </>
+//     );
+// };
 
 const RENDER_COMPONENT = {
     toMobile: MobileReset,
@@ -59,7 +59,9 @@ class ResetPassword extends Component {
         let RenderComponent = RENDER_COMPONENT.default;
 
         if (step === 'init') {
-            RenderComponent = currentLanguage === 'zh-CN' ? ResetInit : MailReset;
+            // TODO 暂时隐去邮箱找回功能
+            // RenderComponent = currentLanguage === 'zh-CN' ? ResetInit : MailReset;
+            RenderComponent = currentLanguage === 'zh-CN' ? MobileReset : MailReset;
         } else {
             RenderComponent = RENDER_COMPONENT[step];
         }
