8e2b3effb90850416104d9a7be678891724e031e
Konata9 <maochichen@sunmi.com>
2019-05-15 18:00:52 +0800

    1. 添加全部重推功能

--- src/locales/zh-CN/ESL/device.js
+++ src/locales/zh-CN/ESL/device.js
@@ -85,6 +85,10 @@ export default {
     'esl.device.esl.push.success': '推送成功',
     'esl.device.esl.push.fail': '推送失败',
 
+    'esl.device.esl.week.fail': '近一周推送失败',
+    'esl.device.esl.push.all': '立即重推',
+    'esl.device.esl.push.all.success': '正在重推失败价签...',
+
     // 模板用
     'esl.device.template.new': '新建模板',
     'esl.device.template.id': '模板ID',
