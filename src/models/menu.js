67b02f4435e606e8f72430872aabbf27b5340088
Konata9 <maochichen@sunmi.com>
2019-06-11 19:22:06 +0800

    修复 bugs

--- src/models/menu.js
+++ src/models/menu.js
@@ -97,7 +97,7 @@ export default {
 	namespace: 'menu',
 
 	state: {
-		menuData: Storage.get('FILTERED_MENU', 'session') || [],
+		menuData: Storage.get('FILTERED_MENU', 'local') || [],
 		breadcrumbNameMap: {},
 	},
 
@@ -113,7 +113,7 @@ export default {
 				const { menu_list: menuList = [] } = response.data || {};
 				if (menuList && menuList.length > 0) {
 					filteredMenuData = checkMenuAuth(menuData, menuList);
-					Storage.set({ FILTERED_MENU: filteredMenuData }, 'session');
+					Storage.set({ FILTERED_MENU: filteredMenuData }, 'local');
 				}
 			}
 
