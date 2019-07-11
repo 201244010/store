9940cc74adc92e36ab4e6480708bae790287dc5a
Konata9 <maochichen@sunmi.com>
2019-07-11 17:37:22 +0800

    1. 添加 jest 测试相关配置

--- jest.config.js
+++ jest.config.js
@@ -1,4 +1,13 @@
 module.exports = {
-  testURL: 'http://localhost:8000',
-  preset: 'jest-puppeteer',
+	// testURL: 'http://localhost:8000',
+	// preset: 'jest-puppeteer',
+	moduleFileExtensions: ['js', '/index.js'],
+	testRegex: '(/tests/.*|(\\.|/)(test|spec))\\.(jsx?|tsx?)$',
+	moduleDirectories:['node_modules'],
+	moduleNameMapper: {
+		'^umi/locale$': '<rootDir>/node_modules/umi-plugin-locale',
+		'^umi-utils$': '<rootDir>/node_modules/umi/lib/utils',
+		'^umi(.*)$': '<rootDir>/node_modules/umi/lib$1',
+		'@/(.*)$': '<rootDir>/src/$1',
+	},
 };
