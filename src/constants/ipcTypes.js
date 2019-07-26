913d8ebf76347740d6c4b9b7bcd6b99a5011bc08
Carl Chen <et_cow@hotmail.com>
2019-07-26 11:34:54 +0800

    1、更新旋转参数列表；

--- src/constants/ipcTypes.js
+++ src/constants/ipcTypes.js
@@ -1,14 +1,47 @@
 export  default {
 	'FS1': {
 		hasFaceid: true,
-		pixelRatio: '16:9'
+		pixelRatio: '16:9',
+		rotate: [{
+			key: 0,
+			value: 0
+		},{
+			key: 1,
+			value: 180
+		}]
 	},
 	'SS1': {
 		hasFaceid: false,
-		pixelRatio: '1:1'
+		pixelRatio: '1:1',
+		rotate: [{
+			key: 0,
+			value: 0
+		},{
+			key: 1,
+			value: 90
+		},{
+			key: 2,
+			value: 180
+		},{
+			key: 3,
+			value: 270
+		}]
 	},
 	'FM010': {
 		hasFaceid: false,
-		pixelRatio: '1:1'
+		pixelRatio: '1:1',
+		rotate: [{
+			key: 0,
+			value: 0
+		},{
+			key: 1,
+			value: 90
+		},{
+			key: 2,
+			value: 180
+		},{
+			key: 3,
+			value: 270
+		}]
 	}
 };
