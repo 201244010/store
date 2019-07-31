5a6e2b3a84464bcdc3d870b0e85363114f325f0a
Konata9 <maochichen@sunmi.com>
2019-07-31 15:43:36 +0800

    1. 添加 empty 样式

--- src/pages/DeviceManagement/ESL/SystemConfig/index.js
+++ src/pages/DeviceManagement/ESL/SystemConfig/index.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { connect } from 'dva';
 import { Card, Select, Form, Button, Switch } from 'antd';
+import DataEmpty from '@/components/BigIcon/DataEmpty';
 import { formatMessage } from 'umi/locale';
 import { FORM_SETTING_LAYOUT } from '@/constants/form';
 import styles from './systemConfig.less';
@@ -83,88 +84,102 @@ class SystemConfig extends Component {
 				style={{ width: '100%' }}
 				loading={loading.effects['eslBaseStation/getNetWorkIdList']}
 			>
-				<Form {...FORM_SETTING_LAYOUT}>
-					<Card
-						title={formatMessage({ id: 'esl.device.config.info' })}
-						bordered={false}
-						className={styles['content-card']}
-					>
-						<div className={styles['display-content']}>
-							<Form.Item label={formatMessage({ id: 'esl.device.config.networkId' })}>
-								{getFieldDecorator('networkId', {
-									initialValue: (networkIdList[0] || {}).networkId || null,
-								})(
-									<Select onChange={this.handleSelectChange} style={SELECT_STYLE}>
-										{networkIdList.map(netWork => (
-											<Select.Option
-												key={netWork.networkId}
-												value={netWork.networkId}
-											>
-												{netWork.networkId}
-											</Select.Option>
-										))}
-									</Select>
-								)}
-							</Form.Item>
-						</div>
-					</Card>
-					<Card
-						title={formatMessage({ id: 'esl.device.config.setting' })}
-						bordered={false}
-						className={styles['content-card']}
-					>
-						<div className={styles['display-content']}>
-							<Form.Item
-								label={formatMessage({ id: 'esl.device.config.scan.round' })}
-							>
-								{getFieldDecorator('scanPeriod', {
-									initialValue: 15,
-								})(
-									<Select onChange={this.handleSelectChange} style={SELECT_STYLE}>
-										{SCAN_PERIODS.map((period, index) => (
-											<Select.Option key={index} value={period}>
-												{period}
-												{formatMessage({ id: 'countDown.unit' })}
-											</Select.Option>
-										))}
-									</Select>
-								)}
-							</Form.Item>
-							<Form.Item
-								label={formatMessage({ id: 'esl.device.config.scan.green' })}
-							>
-								{getFieldDecorator('isEnergySave', {
-									initialValue: true,
-									valuePropName: 'checked',
-								})(<Switch />)}
-							</Form.Item>
-							<Form.Item label=" " colon={false}>
-								<Button type="primary">{formatMessage({ id: 'btn.save' })}</Button>
-							</Form.Item>
-						</div>
-					</Card>
-					{/* 广播这一期不做 */}
-					{/* <Card
-						title={formatMessage({ id: 'esl.device.config.boardcast' })}
-						bordered={false}
-					>
-						<div className={styles['display-content']}>
-							<Form.Item
-								label={formatMessage({
-									id: 'esl.device.config.boardcast.startTime',
-								})}
-							>
-								{getFieldDecorator('time', {})(<TimePicker />)}
-							</Form.Item>
-							<Form.Item label=" " colon={false}>
-								<Button type="primary">{formatMessage({ id: 'btn.open' })}</Button>
-								<Button className={styles['btn-margin-left']}>
-									{formatMessage({ id: 'btn.close' })}
-								</Button>
-							</Form.Item>
-						</div>
-					</Card> */}
-				</Form>
+				{networkIdList.length > 0 ? (
+					<Form {...FORM_SETTING_LAYOUT}>
+						<Card
+							title={formatMessage({ id: 'esl.device.config.info' })}
+							bordered={false}
+							className={styles['content-card']}
+						>
+							<div className={styles['display-content']}>
+								<Form.Item
+									label={formatMessage({ id: 'esl.device.config.networkId' })}
+								>
+									{getFieldDecorator('networkId', {
+										initialValue: (networkIdList[0] || {}).networkId || null,
+									})(
+										<Select
+											onChange={this.handleSelectChange}
+											style={SELECT_STYLE}
+										>
+											{networkIdList.map(netWork => (
+												<Select.Option
+													key={netWork.networkId}
+													value={netWork.networkId}
+												>
+													{netWork.networkId}
+												</Select.Option>
+											))}
+										</Select>
+									)}
+								</Form.Item>
+							</div>
+						</Card>
+						<Card
+							title={formatMessage({ id: 'esl.device.config.setting' })}
+							bordered={false}
+							className={styles['content-card']}
+						>
+							<div className={styles['display-content']}>
+								<Form.Item
+									label={formatMessage({ id: 'esl.device.config.scan.round' })}
+								>
+									{getFieldDecorator('scanPeriod', {
+										initialValue: 15,
+									})(
+										<Select
+											onChange={this.handleSelectChange}
+											style={SELECT_STYLE}
+										>
+											{SCAN_PERIODS.map((period, index) => (
+												<Select.Option key={index} value={period}>
+													{period}
+													{formatMessage({ id: 'countDown.unit' })}
+												</Select.Option>
+											))}
+										</Select>
+									)}
+								</Form.Item>
+								<Form.Item
+									label={formatMessage({ id: 'esl.device.config.scan.green' })}
+								>
+									{getFieldDecorator('isEnergySave', {
+										initialValue: true,
+										valuePropName: 'checked',
+									})(<Switch />)}
+								</Form.Item>
+								<Form.Item label=" " colon={false}>
+									<Button type="primary">
+										{formatMessage({ id: 'btn.save' })}
+									</Button>
+								</Form.Item>
+							</div>
+						</Card>
+						{/* 广播这一期不做 */}
+						{/* <Card
+									title={formatMessage({ id: 'esl.device.config.boardcast' })}
+									bordered={false}
+								>
+									<div className={styles['display-content']}>
+										<Form.Item
+											label={formatMessage({
+												id: 'esl.device.config.boardcast.startTime',
+											})}
+										>
+											{getFieldDecorator('time', {})(<TimePicker />)}
+										</Form.Item>
+										<Form.Item label=" " colon={false}>
+											<Button type="primary">{formatMessage({ id: 'btn.open' })}</Button>
+											<Button className={styles['btn-margin-left']}>
+												{formatMessage({ id: 'btn.close' })}
+											</Button>
+										</Form.Item>
+									</div>
+								</Card> */}
+					</Form>
+				) : (
+					<DataEmpty />
+				)}
 			</Card>
 		);
 	}
