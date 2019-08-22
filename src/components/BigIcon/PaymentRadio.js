bec3cb02db847bd171a38009496b627bc016b67e
Konata9 <maochichen@sunmi.com>
2019-08-22 15:27:09 +0800

    1. 添加支付假页面 2. 添加 svg

---
+++ src/components/BigIcon/PaymentRadio.js
@@ -0,0 +1,5 @@
+import React from 'react';
+
+const PaymentRadio = () => <div>1234</div>;
+
+export default PaymentRadio;
--- src/pages/Payment/index.js
+++ src/pages/Payment/index.js
@@ -1,8 +1,10 @@
 import React, { PureComponent } from 'react';
 import { connect } from 'dva';
 import { formatMessage } from 'umi/locale';
-import { Card, Table } from 'antd';
+import { Card, Table, Modal, Button, Tabs } from 'antd';
 import styles from './payment.less';
+
+const { TabPane } = Tabs;
 
 const mockData = {
 	orderNumber: 'YCC1234567890213456',
@@ -15,6 +17,18 @@ const mockData = {
 		},
 	],
 };
+
+const NoAuth = ({ visible = false, onCancel = null }) => (
+	<Modal title={null} footer={null} visible={visible} onCancel={onCancel}>
+		<div className={styles['noAuth-wrapper']}>
+			<div>{formatMessage({ id: 'account.no.certify.info1' })}</div>
+			<div>{formatMessage({ id: 'account.no.certify.info2' })}</div>
+			<Button className={styles['noAuth-btn']} type="primary">
+				{formatMessage({ id: 'account.to.certify' })}
+			</Button>
+		</div>
+	</Modal>
+);
 
 const columns = [
 	{
@@ -52,15 +66,80 @@ const columns = [
 
 @connect()
 class Payment extends PureComponent {
+	constructor(props) {
+		super(props);
+		this.state = {
+			modalVisible: false,
+		};
+	}
+
+	closeModal = () => {
+		this.setState({
+			modalVisible: false,
+		});
+	};
+
 	render() {
+		const { modalVisible } = this.state;
+
 		return (
-			<Card title={formatMessage({ id: 'product.info' })}>
-				<div className={styles['order-title']}>
-					{formatMessage({ id: 'order.number' })}：{mockData.orderNumber}
-				</div>
-				<Table columns={columns} dataSource={mockData.table} pagination={false} />
-				{/* <div className={styles['product-info']}></div> */}
-			</Card>
+			<>
+				<Card title={formatMessage({ id: 'product.info' })}>
+					<div className={styles['order-title']}>
+						{formatMessage({ id: 'order.number' })}：{mockData.orderNumber}
+					</div>
+					<Table columns={columns} dataSource={mockData.table} pagination={false} />
+					<div className={styles['product-info']}>
+						<div>
+							<div className={styles['product-wrapper']}>
+								<div className={styles['product-title']}>
+									{formatMessage({ id: 'product.count' })}：
+								</div>
+								<div className={styles['product-content']}>1</div>
+							</div>
+
+							<div className={styles['product-wrapper']}>
+								<div className={styles['product-title']}>
+									{formatMessage({ id: 'product.total' })}：
+								</div>
+								<div className={styles['product-content']}>69</div>
+							</div>
+
+							<div className={styles['product-wrapper']}>
+								<div className={styles['product-title']}>
+									{formatMessage({ id: 'product.discount' })}：
+								</div>
+								<div className={styles['product-content']}>-0.00</div>
+							</div>
+
+							<div className={styles['total-amount']}>¥ 69.00</div>
+						</div>
+					</div>
+				</Card>
+
+				<Card
+					title={formatMessage({ id: 'purchase.choose' })}
+					style={{ marginTop: '20px' }}
+				>
+					<Tabs defaultActiveKey="business" animated={false}>
+						<TabPane tab={formatMessage({ id: 'business.account' })} key="business">
+							Content of Tab Pane 1
+						</TabPane>
+						<TabPane tab={formatMessage({ id: 'person.account' })} key="account">
+							Content of Tab Pane 2
+						</TabPane>
+					</Tabs>
+
+					<div className={styles['action-bar']}>
+						<div className={styles['rest-time']}>
+							{formatMessage({ id: 'purchase.time.rest' })}：
+						</div>
+						<Button type="primary">{formatMessage({ id: 'purchase.intime' })}</Button>
+					</div>
+				</Card>
+
+				<NoAuth visible={modalVisible} onCancel={this.closeModal} />
+			</>
 		);
 	}
 }
