3405a89fe72657d582ae22eafa851d8562de911f
Konata9 <maochichen@sunmi.com>
2019-09-23 10:30:20 +0800

    1. 添加 passengerFlow service

--- src/services/passengerFlow.js
+++ src/services/passengerFlow.js
@@ -1,3 +1,4 @@
+import { format } from '@konata9/milk-shake';
 import { customizeFetch } from '@/utils/fetch';
 
 const fetchApi = customizeFetch('api/passengerFlow');
@@ -10,4 +11,45 @@ export const handlePassengerFlowManagement = (api, options = {}) => {
 	};
 
 	return fetchApi(api, opts).then(response => response.json());
+};
+
+export const getPassengerFlowHistory = options => {
+	const opts = {
+		method: 'POST',
+		body: { ...format('toSnake')(options) },
+	};
+
+	return fetchApi('statistic/history/getList', opts)
+		.then(response => response.json())
+		.then(data => format('toCamel')(data));
+};
+
+export const getPassengerFlowHistoryTrend = options => {
+	const opts = {
+		body: { ...format('toSnake')(options) },
+	};
+
+	return fetchApi('statistic/history/getListByTimeRange', opts)
+		.then(response => response.json())
+		.then(data => format('toCamel')(data));
+};
+
+export const getPassengerFlowAgeByGender = options => {
+	const opts = {
+		body: { ...format('toSnake')(options) },
+	};
+
+	return fetchApi('statistic/age/getListByGender', opts)
+		.then(response => response.json())
+		.then(data => format('toCamel')(data));
+};
+
+export const getPassengerFlowHistoryWithAgeAndGender = options => {
+	const opts = {
+		body: { ...format('toSnake')(options) },
+	};
+
+	return fetchApi('statistic/age/getHistoryWithAgeAndGender', opts)
+		.then(response => response.json())
+		.then(data => format('toCamel')(data));
 };
