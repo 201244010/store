04d92b89f125f8a0f60bff561b6bb23388f6d7b3
Konata9 <maochichen@sunmi.com>
2019-09-23 18:46:53 +0800

    a

--- src/pages/passengerAnalyze/index.js
+++
@@ -1,9 +0,0 @@
-import React, { PureComponent } from '@/pages/PassengerAnalyze/node_modules/react';
-
-class PassengerAnalyze extends PureComponent {
-	render() {
-		return <div>PassengerAnalyze</div>;
-	}
-}
-
-export default PassengerAnalyze;
