3c05adb3a0398bd6481ef090ab8fe8f79ee1e9f3
Konata9 <maochichen@sunmi.com>
2019-10-15 11:12:08 +0800

    1. 修改动画 2. 增加 FS 与 SaaS 对接数据的条件判断

--- src/pages/DashBoard/SalseChart.js
+++ src/pages/DashBoard/SalseChart.js
@@ -3,16 +3,22 @@ import { connect } from 'dva';
 import { formatMessage } from 'umi/locale';
 import { Card, Skeleton } from 'antd';
 import moment from 'moment';
+import { format } from '@konata9/milk-shake';
 import LinePoint from '@/components/Charts/LinePoint';
 import Bar from '@/components/Charts/Bar';
 import { DASHBOARD } from './constants';
-import { salseChartToolTipStyle } from './chartStyle';
 import styles from './DashBoard.less';
 
 const {
 	SEARCH_TYPE: { RANGE },
 	TIME_TICKS,
 } = DASHBOARD;
+
+const CHART_MODE = {
+	TRADE_TRANS: 'tradeTrans',
+	TRADE_BAR: 'tradeBar',
+	CUSTOMER_BAR: 'customerBar',
+};
 
 const TIME_SCALE = {
 	[RANGE.TODAY]: {
@@ -26,15 +32,19 @@ const TIME_SCALE = {
 	},
 };
 
-const CardTitle = ({ loading = true, fsDevice = null }) => (
-	<Skeleton paragraph={false} loading={loading} active>
-		<span>
-			{fsDevice
-				? formatMessage({ id: 'trade.transfer.trend' })
-				: formatMessage({ id: 'trade.transfer.count.trend' })}
-		</span>
-	</Skeleton>
-);
+const CardTitle = ({ loading = true, mode = null }) => {
+	const title = {
+		[CHART_MODE.TRADE_BAR]: formatMessage({ id: 'trade.transfer.count.trend' }),
+		[CHART_MODE.CUSTOMER_BAR]: formatMessage({ id: 'customer.count.trend' }),
+		default: formatMessage({ id: 'trade.transfer.trend' }),
+	};
+
+	return (
+		<Skeleton paragraph={false} loading={loading} active>
+			<span>{title[mode] || title.default}</span>
+		</Skeleton>
+	);
+};
 
 const formatXLabel = (value, rangeType, timeRangeStart, timeRangeEnd) => {
 	if (rangeType === RANGE.TODAY) {
@@ -63,21 +73,49 @@ const formatXLabel = (value, rangeType, timeRangeStart, timeRangeEnd) => {
 	return value;
 };
 
-@connect(({ ipcList, dashboard }) => ({
-	ipcList,
-	passengerOrderLoading: dashboard.passengerOrderLoading,
-	passengerOrderList: dashboard.passengerOrderList,
-	searchValue: dashboard.searchValue,
-}))
+@connect(
+	({ ipcList, dashboard }) => ({
+		ipcList,
+		passengerOrderLoading: dashboard.passengerOrderLoading,
+		passengerOrderList: dashboard.passengerOrderList,
+		searchValue: dashboard.searchValue,
+	}),
+	dispatch => ({
+		getShopIdFromStorage: () => dispatch({ type: 'global/getShopIdFromStorage' }),
+		getShopListFromStorage: () => dispatch({ type: 'global/getShopListFromStorage' }),
+	})
+)
 class SalseChart extends PureComponent {
+	async componentDidMount() {
+		const { ipcList, getShopIdFromStorage, getShopListFromStorage } = this.props;
+		const currentShopId = await getShopIdFromStorage();
+		const shopList = await getShopListFromStorage();
+
+		// console.log(shopList.filter(shop => shop.saas_exist === 1));
+		const currentShopInfo = format('toCamel')(
+			shopList.find(shop => format('toCamel')(shop).shopId === currentShopId) || {}
+		);
+		// console.log(currentShopInfo);
+		this.hasFsDevice = ipcList.some(ipc => ipc.hasFaceid);
+		this.saasExist = currentShopInfo.saasExist || 0;
+
+		this.chartMode = CHART_MODE.TRADE_TRANS;
+		if (this.hasFsDevice && !this.saasExist) {
+			this.chartMode = CHART_MODE.CUSTOMER_BAR;
+		} else if (!this.hasFsDevice && this.saasExist) {
+			this.chartMode = CHART_MODE.TRADE_BAR;
+		}
+		console.log('has fs: ', this.hasFsDevice);
+		console.log('has saas: ', this.saasExist);
+		console.log('chart mode:', this.chartMode);
+	}
+
 	render() {
 		const {
-			ipcList = [],
 			searchValue: { rangeType, timeRangeStart, timeRangeEnd },
 			passengerOrderLoading,
 			passengerOrderList,
 		} = this.props;
-		const fsDevice = ipcList.some(ipc => ipc.hasFaceid);
 
 		let chartScale = {
 			time: {
@@ -101,14 +139,119 @@ class SalseChart extends PureComponent {
 		};
 		let chartTip = {};
 		let itemTpl = '';
-		if (fsDevice) {
-			const passengerTicks = { minLimit: 0, maxLimit: 100, ticks: [0, 20, 40, 60, 80, 100] };
+		let DataChart = () => <div />;
 
+		if (this.chartMode === CHART_MODE.TRADE_BAR) {
+			// 无 FS，有 SaaS => 交易笔数柱状图
+			const maxOrderCount = Math.max(...passengerOrderList.map(i => i.orderCount)) || 0;
+			const orderTicks =
+				maxOrderCount < 5
+					? { ticks: [1, 2, 3, 4, 5] }
+					: { tickInterval: Math.ceil(maxOrderCount / 5) };
+
+			chartScale = {
+				...chartScale,
+				orderCount: {
+					minLimit: 0,
+					...orderTicks,
+				},
+			};
+
+			itemTpl = `
+				<li class="g2-tooltip-list-item" data-index={index} style="color:#ffffff;">
+					<div>${formatMessage({ id: 'salse.time' })}: {time}</div>
+					<div>${formatMessage({ id: 'salse.count' })}: {orderCount}</div>
+				</li>
+			`;
+
+			chartTip = [
+				'time*orderCount',
+				(time, orderCount) => ({
+					time: formatXLabel(time, rangeType, timeRangeStart, timeRangeEnd),
+					orderCount,
+				}),
+			];
+
+			DataChart = () => (
+				<Bar
+					{...{
+						chartStyle: {
+							scale: chartScale,
+						},
+						axis: {
+							x: 'time',
+							y: 'orderCount',
+							xLabel: { formatter: val => formatXLabel(val, rangeType) },
+						},
+						tooltip: { itemTpl, useHtml: true },
+						dataSource: passengerOrderList,
+						barStyle: {
+							barActive: true,
+							position: 'time*orderCount',
+							tooltip: chartTip,
+						},
+					}}
+				/>
+			);
+		} else if (this.chartMode === CHART_MODE.CUSTOMER_BAR) {
+			// 有 FS，无 SaaS => 客流柱状图
+			const maxFlowCount =
+				Math.max(...passengerOrderList.map(i => i.passengerFlowCount)) || 0;
+			const flowTicks =
+				maxFlowCount < 5
+					? { ticks: [1, 2, 3, 4, 5] }
+					: { tickInterval: Math.ceil(maxFlowCount / 5) };
+
+			chartScale = {
+				...chartScale,
+				passengerFlowCount: {
+					minLimit: 0,
+					...flowTicks,
+				},
+			};
+
+			itemTpl = `
+			<li class="g2-tooltip-list-item" data-index={index} style="color:#ffffff;">
+				<div>${formatMessage({ id: 'salse.time' })}: {time}</div>
+				<div>${formatMessage({ id: 'customer.count' })}: {passengerFlowCount}</div>
+			</li>
+		`;
+
+			chartTip = [
+				'time*passengerFlowCount',
+				(time, passengerFlowCount) => ({
+					time: formatXLabel(time, rangeType, timeRangeStart, timeRangeEnd),
+					passengerFlowCount,
+				}),
+			];
+
+			DataChart = () => (
+				<Bar
+					{...{
+						chartStyle: {
+							scale: chartScale,
+						},
+						axis: {
+							x: 'time',
+							y: 'passengerFlowCount',
+							xLabel: { formatter: val => formatXLabel(val, rangeType) },
+						},
+						tooltip: { itemTpl, useHtml: true },
+						dataSource: passengerOrderList,
+						barStyle: {
+							barActive: true,
+							position: 'time*passengerFlowCount',
+							tooltip: chartTip,
+						},
+					}}
+				/>
+			);
+		} else {
+			const passengerTicks = { minLimit: 0, maxLimit: 100, ticks: [0, 20, 40, 60, 80, 100] };
 			chartScale = {
 				...chartScale,
 				passengerFlowRate: passengerTicks,
 			};
-
 			itemTpl = `
 				<li class="g2-tooltip-list-item" data-index={index} style="color:#ffffff;">
 					<div>${formatMessage({ id: 'salse.time' })}: {time}</div>
@@ -127,105 +270,53 @@ class SalseChart extends PureComponent {
 					passengerFlowCount,
 				}),
 			];
-		} else {
-			const maxOrderCount = Math.max(...passengerOrderList.map(i => i.orderCount)) || 0;
-			const orderTicks =
-				maxOrderCount < 5
-					? { ticks: [1, 2, 3, 4, 5] }
-					: { tickInterval: Math.ceil(maxOrderCount / 5) };
 
-			chartScale = {
-				...chartScale,
-				orderCount: {
-					minLimit: 0,
-					...orderTicks,
-				},
-			};
+			DataChart = () => (
+				<LinePoint
+					{...{
+						data: passengerOrderList,
+						scale: chartScale,
+						tooltip: { itemTpl, useHtml: true },
+						axis: {
+							x: {
+								name: 'time',
+								label: {
+									formatter: val =>
+										formatXLabel(val, rangeType, timeRangeStart, timeRangeEnd),
+								},
+							},
+							y: {
+								name: 'passengerFlowRate',
+								label: {
+									formatter: val => `${val}%`,
+								},
+							},
+						},
+						line: {
+							position: 'time*passengerFlowRate',
+							color: ['passengerFlowRate', '#25B347'],
+							tooltip: chartTip,
+							shape: '',
+						},
+						point: {
+							position: 'time*passengerFlowRate',
+							color: ['passengerFlowRate', '#25B347'],
+							tooltip: chartTip,
+						},
+					}}
+				/>
+			);
 		}
 
 		return (
 			<Card
 				loading={passengerOrderLoading}
-				title={<CardTitle loading={passengerOrderLoading} fsDevice={fsDevice} />}
+				title={<CardTitle loading={passengerOrderLoading} mode={this.chartMode} />}
 				className={`${styles['card-bar-wrapper']}  ${
-					passengerOrderLoading || !fsDevice ? '' : styles['salse-chart']
+					passengerOrderLoading ? '' : styles['salse-chart']
 				}`}
 			>
-				{fsDevice ? (
-					passengerOrderList.length > 0 && (
-						<LinePoint
-							{...{
-								data: passengerOrderList,
-								scale: chartScale,
-								tooltip: { itemTpl, useHtml: true },
-								axis: {
-									x: {
-										name: 'time',
-										label: {
-											formatter: val =>
-												formatXLabel(
-													val,
-													rangeType,
-													timeRangeStart,
-													timeRangeEnd
-												),
-										},
-									},
-									y: {
-										name: 'passengerFlowRate',
-										label: {
-											formatter: val => `${val}%`,
-										},
-									},
-								},
-								line: {
-									position: 'time*passengerFlowRate',
-									color: ['passengerFlowRate', '#25B347'],
-									tooltip: chartTip,
-									shape: '',
-								},
-								point: {
-									position: 'time*passengerFlowRate',
-									color: ['passengerFlowRate', '#25B347'],
-									tooltip: chartTip,
-								},
-							}}
-						/>
-					)
-				) : (
-					<Bar
-						{...{
-							chartStyle: {
-								scale: chartScale,
-							},
-							axis: {
-								x: 'time',
-								y: 'orderCount',
-								xLabel: { formatter: val => formatXLabel(val, rangeType) },
-							},
-							dataSource: passengerOrderList,
-							barStyle: {
-								barActive: true,
-								position: 'time*orderCount',
-								tooltip: [
-									'time*orderCount',
-									(time, orderCount) => ({
-										name: `${formatMessage({
-											id: 'salse.time',
-										})}:${formatXLabel(
-											time,
-											rangeType,
-											timeRangeStart,
-											timeRangeEnd
-										)}`,
-										value: orderCount,
-									}),
-								],
-							},
-							toolTipStyle: salseChartToolTipStyle,
-						}}
-					/>
-				)}
+				<DataChart />
 			</Card>
 		);
 	}
