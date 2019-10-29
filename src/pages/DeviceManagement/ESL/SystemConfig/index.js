6885a3bb5dfed9720c225a53aa6ac4334e2a2bd2
fdjangwu <fdjiangwu@yeah.net>
2019-10-29 13:52:02 +0800

    10.29 发版 隐藏功能

--- src/pages/DeviceManagement/ESL/SystemConfig/index.js
+++ src/pages/DeviceManagement/ESL/SystemConfig/index.js
@@ -1,7 +1,7 @@
 import React, {Component} from 'react';
 import {Card, Tabs} from 'antd';
 import { formatMessage } from 'umi/locale';
-import DisplayConfig from './DisplayConfig';
+// import DisplayConfig from './DisplayConfig';
 import DeviceConfig from './DeviceConfig';
 
 const {TabPane} = Tabs;
@@ -11,9 +11,9 @@ export default class SystemConfig extends Component {
 		return (
 			<Card>
 				<Tabs defaultActiveKey="1" animated={false}>
-					<TabPane tab={formatMessage({id: 'esl.device.display.title'})} key="1">
-						<DisplayConfig />
-					</TabPane>
+					{/* <TabPane tab={formatMessage({id: 'esl.device.display.title'})} key="1"> */}
+					{/* <DisplayConfig /> */}
+					{/* </TabPane> */}
 					<TabPane tab={formatMessage({id: 'esl.device.config.title'})} key="2">
 						<DeviceConfig />
 					</TabPane>
