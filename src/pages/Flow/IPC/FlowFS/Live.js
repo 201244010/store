3dbc7d3ebbf4730307f4cfcaa0699a94d098eb81
zhenshuyi <zhenshuyi@sunmi.com>
2019-10-29 17:56:41 +0800

    1.  大屏卡通头像更换

--- src/pages/Flow/IPC/FlowFS/Live.js
+++ src/pages/Flow/IPC/FlowFS/Live.js
@@ -8,8 +8,8 @@ import PerfectScrollbar from 'react-perfect-scrollbar';
 import Faceid from '@/pages/Flow/VideoPlayer/Faceid';
 import LivePlayer from '@/pages/Flow/VideoPlayer/LivePlayer';
 import { LIBRARY_STYLE } from './libraryName';
-import manImage from '@/assets/imgs/man.png';
-import womanImage from '@/assets/imgs/woman.png';
+import manImage from '@/assets/imgs/screen-male.png';
+import womanImage from '@/assets/imgs/screen-female.png';
 
 import styles from './Live.less';
 
@@ -208,7 +208,6 @@ class Live extends React.Component{
 		const sn = this.getSN();
 		let sdStatus = true;
 		if (sn) {
-			// test();
 			clearList({ sn });
 			getAgeRangeList();
 			await loadList();
@@ -227,6 +226,7 @@ class Live extends React.Component{
 				// setTimeout(() => {
 				// 	this.startFaceComparePush();
 				// }, 3000);
+				// test();
 
 			}
 
