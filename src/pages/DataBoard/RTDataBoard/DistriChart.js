11d3adbe120096171a8ea3d872e696507a24f185
zhenshuiyi <zhenshuiyi@sunmi.com>
2020-03-28 13:06:53 +0800

    1. fix 分布图的布局

--- src/pages/DataBoard/RTDataBoard/DistriChart.js
+++ src/pages/DataBoard/RTDataBoard/DistriChart.js
@@ -1,5 +1,5 @@
 import React, { PureComponent } from 'react';
-import { Card } from 'antd';
+import { Card, Row, Col } from 'antd';
 import { formatMessage } from 'umi/locale';
 import Pie from '../Charts/Pie/Pie';
 import DataAgeGender from '../Charts/Bars/AgeGenderBar';
@@ -14,14 +14,21 @@ class DistriChart extends PureComponent {
 		const colorArray = ['rgba(255, 128, 0, 1)', 'rgba(90, 151, 252, 1)'];
 		return(
 			<Card title={formatMessage({ id: 'databoard.passenger.distri.title'})} className={styles['distri-chart-wrapper']} loading={loading}>
-				<div className={styles['regular-chart']}>
-					<h4 className={styles['regular-title']}>{formatMessage({ id: 'databoard.passenger.regular.title'})}</h4>
-					<Pie data={regularList} colorArray={colorArray} />
-				</div>
-				<div className={styles['age-chart']}>
-					{/* <h4>性别年龄占比</h4> */}
-					<DataAgeGender ageGenderList={ageGenderList} />
-				</div>
+				<Row>
+					<Col span={8}>
+						<div className={styles['regular-chart']}>
+							<h4 className={styles['regular-title']}>{formatMessage({ id: 'databoard.passenger.regular.title'})}</h4>
+							<Pie data={regularList} colorArray={colorArray} />
+						</div>
+					</Col>
+					<Col span={16}>
+						<DataAgeGender ageGenderList={ageGenderList} />
+					</Col>
+				</Row>
+				
+				{/* <div className={styles['age-chart']}>
+					<h4>性别年龄占比</h4>
+				</div> */}
 			</Card>
 		);
 	}
